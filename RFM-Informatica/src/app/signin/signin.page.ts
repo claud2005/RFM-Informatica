7cf1ee86f8ffa269634f238894b19d2a4b513f6e
claud2005 <162583107+claud2005@users.noreply.github.com>
2025-02-25 09:22:29 +0000

    ..
    .....

--- RFM-Informatica/src/app/signin/signin.page.ts
+++ RFM-Informatica/src/app/signin/signin.page.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule],
   templateUrl: './signin.page.html',
   styleUrls: ['./signin.page.scss'],
 })
-export class SigninPage implements OnInit {
+export class SigninPage {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
